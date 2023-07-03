f112bcdb39e86ff95cabcce68be5d4e847d8207a
polivera <punk.moon2229@fastmail.com>
2023-07-03 14:21:32 +0200

    Adding orderless, marginalia and embark

--- init.el
+++ init.el
@@ -85,6 +85,60 @@
   (setq consult-project-root-function #'projectile-project-root)
 )
 
+(use-package orderless
+  :straight t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+:straight t
+;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+;; available in the *Completions* buffer, add it to the
+;; `completion-list-mode-map'.
+:bind (:map minibuffer-local-map
+       ("M-A" . marginalia-cycle))
+;; The :init section is always executed.
+:init
+
+;; Marginalia must be actived in the :init section of use-package such that
+;; the mode gets enabled right away. Note that this forces loading the
+;; package.
+(marginalia-mode))
+
+(use-package embark
+  :straight t
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc.  You may adjust the Eldoc
+  ;; strategy, if you want to see the documentation from multiple providers.
+  (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :straight t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
 ;; Doom Modeline
 (use-package doom-modeline
   :straight t
